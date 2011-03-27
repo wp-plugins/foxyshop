a9731aaefdf925942b3f4056fa5402df7871c82a
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-27 23:31:17 +0000

    Version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/foxyshop/tags/2.1@365594 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themefiles/foxyshop-product-loop.php
@@ -0,0 +1,52 @@
+<?php
+/***************************
+Product display details
+referenced in:
+- foxyshop-all-products.php
+- foxyshop-search.php
+- foxyshop-single-category.php
+****************************/
+
+//Setup Product
+global $product, $post;
+$product = foxyshop_setup_product();
+if ($product['hide_product']) continue;
+
+
+echo '<li class="foxyshop_product_box">';
+
+//Show Image on Left
+echo '<div class="foxyshop_product_image">';
+if ($thumbnailSRC = foxyshop_get_main_image("thumbnail")) echo '<a href="' . $product['url'] . '"><img src="' . $thumbnailSRC . '" alt="' . htmlspecialchars($product['name']) . '" class="foxyshop_main_image" /></a>';
+echo "</div>\n";
+
+//Show Main Product Info
+echo '<div class="foxyshop_product_info">';
+echo '<h2><a href="' . $product['url'] . '">' . apply_filters('the_title', $product['name']) . '</a></h2>';
+
+//Show a sale tag if the product is on sale
+//if (foxyshop_is_on_sale()) echo '<p>SALE!</p>';
+
+//Product Is New Tag
+//if (foxyshop_is_product_new(14)) echo '<p>NEW!</p>';
+
+if ($product['short_description']) echo "<p>" . $product['short_description'] . "</p>";
+
+//More Details Button
+echo '<a href="' . $product['url'] . '" class="foxyshop_button">More Details</a>';
+
+//Add To Cart Button (options)
+//foxyshop_product_link("Add To Cart", false);
+//foxyshop_product_link("Add %name% To Cart", false);
+//echo '<a href="'.foxyshop_product_link("", true).'" class="foxyshop_button">Add To Cart</a>';
+
+//Show Price (and sale if applicable)
+foxyshop_price();
+
+echo "</div>\n";
+
+//Clear Floats and End Item
+echo '<div class="clr"></div>';
+echo "</li>\n";
+
+?>
