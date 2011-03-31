e4395a37f5f9f922e995ec0fcd76cc437068dab9
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-31 16:48:30 +0000

    Version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/foxyshop/tags/2.2@367602 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ foxyshop.php
@@ -0,0 +1,146 @@
+<?php /*
+
+**************************************************************************
+Plugin Name: FoxyShop
+Plugin URI: http://www.foxy-shop.com/
+Description: FoxyShop is a full integration for FoxyCart and WordPress, providing a robust shopping cart and inventory management tool.
+Author: SparkWeb Interactive, Inc.
+Version: 2.2
+Author URI: http://www.foxy-shop.com/
+
+**************************************************************************
+
+Copyright (C) 2011 SparkWeb Interactive, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************
+
+Thanks you for using this plugin. Please see http://www.foxy-shop.com/ for 
+installation instructions and lots of helpful advice on how to get
+the most out of FoxyShop.
+
+**************************************************************************/
+
+//Setup Plugin Variables
+define('FOXYSHOP_DIR',WP_PLUGIN_URL."/foxyshop");
+define('FOXYSHOP_PATH', dirname(__FILE__));
+if (!defined('FOXYSHOP_PRODUCTS_SLUG')) define('FOXYSHOP_PRODUCTS_SLUG','products');
+if (!defined('FOXYSHOP_PRODUCT_CATEGORY_SLUG')) define('FOXYSHOP_PRODUCT_CATEGORY_SLUG','product-cat');
+$foxyshop_settings_defaults = array(
+	'sort_key' => "menu_order",
+	'generate_feed' => "",
+	"products_per_page" => -1,
+	"hide_subcat_children" => "on",
+	"generate_product_sitemap" => "",
+	"manage_inventory_levels" => "",
+	"datafeed_url_key" => "",
+	"inventory_alert_level" => 3,
+	"ga_advanced" => "",
+	"enable_sso" => "",
+	"sso_account_required" => "",
+	"browser_title_1" => "Products | " . get_bloginfo("name"),
+	"browser_title_2" => "Product Categories | " . get_bloginfo("name"),
+	"browser_title_3" => "%c | " . get_bloginfo("name"),
+	"browser_title_4" => "%p | " . get_bloginfo("name"),
+	"browser_title_5" => "Product Search | " . get_bloginfo("name")
+);
+$foxyshop_settings = wp_parse_args(unserialize(get_option("foxyshop_settings")), $foxyshop_settings_defaults);
+$foxyshop_category_sort = (get_option('foxyshop_category_sort') ? unserialize(get_option('foxyshop_category_sort')) : array());
+if ($foxyshop_settings['version'] == "0.70") $foxyshop_settings['version'] = "0.7.0";
+if (array_key_exists('inventory_url_key', $foxyshop_settings)) $foxyshop_settings['datafeed_url_key'] = $foxyshop_settings['inventory_url_key'];
+
+//Sets the Locale for Currency Internationalization
+setlocale(LC_MONETARY, get_locale());
+$foxyshop_localsettings = localeconv();
+if ($foxyshop_localsettings['int_curr_symbol'] == "") setlocale(LC_MONETARY, 'en_US');
+
+//Widgets and Shortcodes support
+include('widgetcode.php');
+include('shortcodesettings.php');
+
+//Put FoxyCart includes and jQuery on public pages
+if (!is_admin()) {
+	if ($foxyshop_settings['use_jquery']) add_action('init', 'foxyshop_insert_jquery');
+	add_action('wp_head', 'foxyshop_insert_foxycart_files');
+}
+
+//Put FoxyShop Styles on front pages and load in the admin styles as well
+if (is_admin()) {
+	wp_enqueue_style('foxyshop_admin_css', FOXYSHOP_DIR . '/css/foxyshop-admin.css');
+} else {
+	wp_enqueue_style('foxyshop_css', FOXYSHOP_DIR . '/css/foxyshop.css');
+	if ($foxyshop_settings['ga']) add_action('wp_footer', 'foxyshop_insert_google_analytics', 100);
+}
+
+//Custom Post Type and Taxonomy
+include_once('customposttype.php');
+
+//Custom Product Sorting
+include_once('customsorting.php');
+
+//Custom Category Sorting
+include_once('categorysorting.php');
+
+//FoxyCart API Feeds
+if ($foxyshop_settings['domain']) {
+
+	//Orders
+	include_once('orders.php');
+
+	//Customers
+	include_once('customers.php');
+
+	//Subscriptions
+	if ($foxyshop_settings['enable_subscriptions']) {
+		include_once('subscriptions.php');
+	}
+}
+
+//Inventory Management
+if ($foxyshop_settings['manage_inventory_levels']) {
+	include_once('inventory.php');
+}
+
+//Generate Product Feed
+if ($foxyshop_settings['generate_feed']) {
+	include_once('productfeed.php');
+}
+
+//Settings Page
+include_once('settings.php');
+
+//Single Sign On
+if ($foxyshop_settings['enable_sso']) {
+	include_once('sso.php');
+}
+
+//Display Settings Link on Plugin Screen
+add_filter('plugin_action_links', 'foxyshop_plugin_action_links', 10, 2);
+
+//Admin Functions
+include('adminfunctions.php');
+include('adminajax.php');
+
+//Frontend Helper Functions
+include('helperfunctions.php');
+
+//Template Redirector (files are in /themefiles/)
+include('templateredirect.php');
+
+//Plugin Activation Functions
+register_activation_hook(__FILE__, 'foxyshop_activation');
+register_deactivation_hook( __FILE__, 'foxyshop_deactivation' );
+?>
---
+++ js/variation.process.jquery.js
@@ -0,0 +1,136 @@
+jQuery(document).ready(function($){
+	$("form.foxyshop_product select, form.foxyshop_product input:checkbox, form.foxyshop_product input:radio").change(function(){
+		updateVariations($(this));
+	});
+	updateVariations($("form.foxyshop_product select, form.foxyshop_product input:checkbox, form.foxyshop_product input:radio"));
+
+	function updateVariations(elSelect) {
+		price = $("#price").val();
+		price1 = $("#originalprice").val();
+		price = parseFloat(price.replace(",","")) * 100;
+		price1 = parseFloat(price1.replace(",","")) * 100;
+		displayKey = new Array();
+		elSelect.parents("form").find(".foxyshop_variations select option:selected, .foxyshop_variations input:checkbox:checked, .foxyshop_variations input:radio:checked").each(function(){
+			var thisEl = $(this);
+			var selectedValue = thisEl.val();
+			
+			//Set Image Key
+			imagekey = thisEl.attr("imagekey");
+			if (imagekey != "" && typeof imagekey != "undefined") {
+				for (i=0; i<ikey.length; i++) {
+					if (ikey[i][0] == imagekey) {
+						$("#foxyshop_main_product_image").attr("src",ikey[i][2]).attr("alt",ikey[i][4]).parent().attr("href",ikey[i][3]);
+						$("#foxyshop_cart_product_image").attr("name",'image'+ikey[i][5]).val(ikey[i][1]);
+					}
+				}
+			}
+
+			//Check Inventory
+			varcode = thisEl.attr("code");
+			if (varcode != "" && typeof varcode != "undefined" && typeof arr_foxyshop_inventory != 'undefined') {
+				match = 0;
+				for (i=0; i<arr_foxyshop_inventory.length; i++) {
+					if (arr_foxyshop_inventory[i][0] == varcode) {
+						if (arr_foxyshop_inventory[i][1] > 0) {
+							$(".foxyshop_stock_alert").removeClass("foxyshop_out_of_stock").text(update_inventory_alert_language(foxyshop_inventory_stock_alert,arr_foxyshop_inventory[i][1])).show();
+							$("#productsubmit").removeAttr("disabled").removeClass("foxyshop_disabled");
+						} else {
+							$(".foxyshop_stock_alert").addClass("foxyshop_out_of_stock").text(update_inventory_alert_language(foxyshop_inventory_stock_none,arr_foxyshop_inventory[i][1])).show();
+							if (!foxyshop_allow_backorder) $("#productsubmit").attr("disabled","disabled").addClass("foxyshop_disabled");
+						}
+						match = 1;
+					}
+				}
+				if (match == 0) {
+					$("#productsubmit").removeAttr("disabled").removeClass("foxyshop_disabled");
+					$(".foxyshop_stock_alert").removeClass("foxyshop_out_of_stock").hide();
+				}
+			}
+
+
+			//Set Display Key
+			thisdisplaykey = thisEl.attr("displaykey");
+			if (thisdisplaykey != "") displayKey[displayKey.length] = thisdisplaykey;
+
+			//Price Change
+			priceChange = thisEl.attr("pricechange");
+			priceSet = thisEl.attr("priceset");
+			if (priceChange) {
+				priceChangeAmount = parseFloat(priceChange);
+				if (priceChange.substr(1,2) == "-") {
+					price = price - priceChangeAmount;
+					price1 = price1 - priceChangeAmount;
+				} else {
+					price = price + priceChangeAmount;
+					price1 = price1 + priceChangeAmount;
+				}
+			} else if (priceSet) {
+				price = parseFloat(priceSet);
+				price1 = price;
+			}
+		});
+		
+		$(".dkey").hide();
+		for (i=0;i<displayKey.length;i++) {
+			$(".dkey[dkey=" + displayKey[i] + "]").show();
+		}
+		$(".dkey:hidden").each(function() {
+			var thisEl = $(this);
+			if (thisEl.is('input') || $(this).is('textarea')) {
+				thisEl.val("");
+			} else if ($(this).is('select')) {
+				thisEl.attr('selectedIndex', '-1');
+			}
+
+		});
+		
+		l18n_settings = $("#foxyshop_l18n").val();
+		arrl18n_settings = l18n_settings.split("|")
+		currencySymbol = arrl18n_settings[0];
+		decimalSeparator = arrl18n_settings[1];
+		thousandsSeparator = arrl18n_settings[2];
+		p_precedes = arrl18n_settings[3];
+		n_sep_by_space = arrl18n_settings[4];
+		$("#foxyshop_main_price .foxyshop_currentprice").text(toCurrency(price, currencySymbol, thousandsSeparator, decimalSeparator, p_precedes, n_sep_by_space));
+		$("#foxyshop_main_price .foxyshop_oldprice").text(toCurrency(price1, currencySymbol, thousandsSeparator, decimalSeparator, p_precedes, n_sep_by_space));
+	
+		function update_inventory_alert_language(strlang,itemcount) {
+			strlang = strlang.replace('%c',itemcount);
+			if (itemcount == 1) {
+				strlang = strlang.replace('%s',"");
+			} else {
+				strlang = strlang.replace('%s',"s");
+			}
+			strlang = strlang.replace('%n',$("#input[name^='name||']").val());
+			return strlang;
+		}
+
+	}
+	
+	function toCurrency(n, c, g, d, first, separator) {
+		var s = (0 > n) ? '-' : '';
+		if (separator == 1) { separator = ' '; } else { separator = ''; }
+		var m = String(Math.round(Math.abs(n)));
+		var i = '', j, f; c = c || ''; g = g || ''; d = d || '.';
+		while(m.length < 3) {m = '0' + m;}
+		f = m.substring((j = m.length - 2));
+		while(j > 3) {
+			i = g + m.substring(j - 3, j) + i;
+			j -= 3;
+		}
+		i = m.substring(0, j) + i;
+		if (first == 1) {
+			return s + c + separator + i + d + f;
+		} else {
+			return s + i + d + f + separator + c;
+		}
+	}
+
+});
+
+function foxyshop_is_array(obj) {
+	if (obj.constructor.toString().indexOf("Array") == -1)
+		return false;
+	else
+		return true;
+}
