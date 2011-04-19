5e8c9819c87fd3ceacfab6f8c826782d24cf828b
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-19 22:24:21 +0000

    Version 2.3.1
    git-svn-id: https://plugins.svn.wordpress.org/foxyshop/tags/2.3.1@374993 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/uploadify/uploadify_admin.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Uploadify v2.1.4
+Release Date: November 8, 2010
+
+Copyright (c) 2009 Ronnie Garcia, Travis Nickels
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+if (!empty($_FILES)) {
+	
+	$foxyshop_document_root = $_SERVER['DOCUMENT_ROOT'];
+	if ($foxyshop_document_root == "" || $foxyshop_document_root == "/") $foxyshop_document_root = str_replace("/wp-content/plugins/foxyshop/js/uploadify","",dirname(__FILE__));
+
+	
+	$tempFile = $_FILES['Filedata']['tmp_name'];
+	$targetPath = realpath($foxyshop_document_root) . '/'.$_REQUEST['folder'].'/';
+	$targetPath = str_replace('//','/',$targetPath);
+	
+	$newfilename = urldecode($_FILES['Filedata']['name']);
+	$newfilename = str_replace('[1]','',$newfilename);
+	$newfilename = str_replace('[2]','',$newfilename);
+	$newfilename = str_replace('[3]','',$newfilename);
+	$newfilename = sanitize_file_name($newfilename);
+	
+	$targetFile =  $targetPath . $newfilename;
+
+	$ext = strtolower(substr($newfilename, strrpos($newfilename, '.') + 1));
+	if (stristr($ext, '.php')) die('unsupported file type');
+
+	$i = 0;
+	while (file_exists($targetFile)) {
+		$i++;
+		if (stristr($newfilename, '.')) {
+			$ext = preg_replace('/^.*\./', '', $newfilename);
+			$name = substr($newfilename, 0, strrpos($newfilename, '.')) . '_' . $i . '.' . $ext;
+			$targetFile =  $targetPath . $name;
+		} else {
+			$name = $newfilename . '_' . $i . '';
+			$targetFile =  $targetPath . $name;
+		}
+	}
+
+
+	move_uploaded_file($tempFile,$targetFile);
+	echo str_replace($foxyshop_document_root,'',$targetFile);
+} else {
+	echo '1';
+}
+
+function sanitize_file_name( $filename ) {
+	$filename_raw = $filename;
+	$special_chars = array("?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", "\"", "&", "$", "#", "*", "(", ")", "|", "~", "`", "!", "{", "}");
+	$filename = str_replace($special_chars, '', $filename);
+	$filename = preg_replace('/[\s-]+/', '-', $filename);
+	$filename = strtolower(trim($filename, '.-_'));
+	return $filename;
+}
+
+?>
---
+++ themefiles/foxyshop-datafeed-endpoint.php
@@ -0,0 +1,202 @@
+<?php
+//Set Globals and Get Settings
+global $wpdb;
+$foxyshop_settings = unserialize(get_option("foxyshop_settings"));
+
+//Get Post From FoxyCart
+if (isset($_POST["FoxyData"])) {
+    	$FoxyData_received = $_POST["FoxyData"];
+    	$FoxyData_encrypted = urldecode($FoxyData_received);
+	$FoxyData_decrypted = rc4crypt::decrypt($foxyshop_settings['api_key'],$FoxyData_encrypted);
+} else {
+	die('No Content Received');
+}
+
+
+/*
+-------------------------------------------
+SET THIRD-PARTY DATAFEEDS HERE
+-------------------------------------------
+If you need to use more than one datafeed with FoxyCart (let's say you have different types of integrations), you can
+use this template to resend the FoxyData to your other integrations.
+
+Caution: If you are using more than one integration and one of them fails, the entire process will fail and that failure will
+be sent to FoxyCart. They'll then try to submit the datafeed again. So if your integrations aren't checking to see if
+data has already been written in, they could be processing data twice.
+
+If you have more than one additional datafeed and one is more unreliable than another, put the most unreliable one first so that
+any failures will result in a full retry from FoxyCart.
+
+This template will send the WordPress admin an email if any datafeeds fail.
+*/
+
+$third_party_feeds = array(
+	''
+);
+foreach($third_party_feeds as $feedurl) {
+	if ($feedurl) {
+		//Initialize cURL
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $feedurl);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, array("FoxyData" => urlencode($FoxyData_encrypted)));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+		// If you get SSL errors, you can uncomment the following, or ask your host to add the appropriate CA bundle
+		// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = trim(curl_exec($ch));
+
+		//If Error, Send Email and Kill Process
+		if ($response == false || $response != 'foxy') {
+			$error_msg = ($response == false ? "cURL Error: " . curl_error($ch) : $response);
+			$to_email = get_bloginfo('admin_email');
+			$message = "A FoxyCart datafeed error was encountered at " . date("F j, Y, g:i a") . ".\n\n";
+			$message .= "The feed that failed was $feedurl.\n\n";
+			$message .= "The error is listed below:\n\n";
+			$message .= $error_msg;
+			$headers = 'From: ' . get_bloginfo('name') . ' Server Admin <' . $to_email . '>' . "\r\n";
+			wp_mail($to_email, 'Data Feed Error on ' . get_bloginfo('name'), $message, $headers);
+			die($error_msg);
+		}
+		curl_close($ch);
+	}
+}
+/*
+-------------------------------------------
+END THIRD-PARTY DATAFEEDS
+-------------------------------------------
+*/
+
+
+
+
+//Import Response and Parse with SimpleXML
+$xml = simplexml_load_string($FoxyData_decrypted, NULL, LIBXML_NOCDATA);
+
+//For Each Transaction
+foreach($xml->transactions->transaction as $transaction) {
+	
+	//For Each Transaction Detail
+	foreach($transaction->transaction_details->transaction_detail as $transactiondetails) {
+		$product_code = $transactiondetails->product_code;
+		$product_quantity = $transactiondetails->product_quantity;
+		
+		//Get List of Target ID's for Inventory Update
+		$meta_list = $wpdb->get_results("SELECT post_id, meta_id, meta_value FROM $wpdb->postmeta WHERE meta_key = '_inventory_levels' AND meta_value LIKE '%" . str_replace("'","",$product_code) . "%'");
+		foreach ($meta_list as $meta) {
+			$productID = $meta->post_id;
+			$metaID = $meta->meta_id;
+			$val = unserialize(unserialize($meta->meta_value));
+			foreach ($val as $ivcode => $iv) {
+				if ($ivcode == $product_code) {
+					$val[$ivcode]['count'] = $iv['count'] - $product_quantity;
+				}
+			}
+			//Run the Update
+			update_post_meta($productID,"_inventory_levels",serialize($val));
+		}
+		
+		//If you have custom code to run for each product, put it here:
+		
+		
+		
+		
+	}
+}
+die("foxy");
+
+
+
+
+
+
+
+
+// ======================================================================================
+// RC4 ENCRYPTION CLASS
+// Do not modify.
+// ======================================================================================
+/**
+ * RC4Crypt 3.2
+ *
+ * RC4Crypt is a petite library that allows you to use RC4
+ * encryption easily in PHP. It's OO and can produce outputs
+ * in binary and hex.
+ *
+ * (C) Copyright 2006 Mukul Sabharwal [http://mjsabby.com]
+ *     All Rights Reserved
+ *
+ * @link http://rc4crypt.devhome.org
+ * @author Mukul Sabharwal <mjsabby@gmail.com>
+ * @version $Id: class.rc4crypt.php,v 3.2 2006/03/10 05:47:24 mukul Exp $
+ * @copyright Copyright &copy; 2006 Mukul Sabharwal
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @package RC4Crypt
+ */
+ 
+/**
+ * RC4 Class
+ * @package RC4Crypt
+ */
+class rc4crypt {
+	/**
+	 * The symmetric encryption function
+	 *
+	 * @param string $pwd Key to encrypt with (can be binary of hex)
+	 * @param string $data Content to be encrypted
+	 * @param bool $ispwdHex Key passed is in hexadecimal or not
+	 * @access public
+	 * @return string
+	 */
+	function encrypt ($pwd, $data, $ispwdHex = 0)
+	{
+		if ($ispwdHex)
+			$pwd = @pack('H*', $pwd); // valid input, please!
+ 
+		$key[] = '';
+		$box[] = '';
+		$cipher = '';
+ 
+		$pwd_length = strlen($pwd);
+		$data_length = strlen($data);
+ 
+		for ($i = 0; $i < 256; $i++)
+		{
+			$key[$i] = ord($pwd[$i % $pwd_length]);
+			$box[$i] = $i;
+		}
+		for ($j = $i = 0; $i < 256; $i++)
+		{
+			$j = ($j + $box[$i] + $key[$i]) % 256;
+			$tmp = $box[$i];
+			$box[$i] = $box[$j];
+			$box[$j] = $tmp;
+		}
+		for ($a = $j = $i = 0; $i < $data_length; $i++)
+		{
+			$a = ($a + 1) % 256;
+			$j = ($j + $box[$a]) % 256;
+			$tmp = $box[$a];
+			$box[$a] = $box[$j];
+			$box[$j] = $tmp;
+			$k = $box[(($box[$a] + $box[$j]) % 256)];
+			$cipher .= chr(ord($data[$i]) ^ $k);
+		}
+		return $cipher;
+	}
+	/**
+	 * Decryption, recall encryption
+	 *
+	 * @param string $pwd Key to decrypt with (can be binary of hex)
+	 * @param string $data Content to be decrypted
+	 * @param bool $ispwdHex Key passed is in hexadecimal or not
+	 * @access public
+	 * @return string
+	 */
+	function decrypt ($pwd, $data, $ispwdHex = 0)
+	{
+		return rc4crypt::encrypt($pwd, $data, $ispwdHex);
+	}
+}
+
+?>
