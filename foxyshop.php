2c8718df77c758765599590ae03b131829419df7
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-15 17:05:22 +0000

    2.8
    git-svn-id: https://plugins.svn.wordpress.org/foxyshop/tags/2.8@410354 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ foxyshop.php
@@ -0,0 +1,147 @@
+<?php /*
+
+**************************************************************************
+Plugin Name: FoxyShop
+Plugin URI: http://www.foxy-shop.com/
+Description: FoxyShop is a full integration for FoxyCart and WordPress, providing a robust shopping cart and inventory management tool.
+Author: SparkWeb Interactive, Inc.
+Version: 2.8
+Author URI: http://www.foxy-shop.com/
+
+**************************************************************************
+
+Copyright (C) 2011 SparkWeb Interactive, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************
+
+Thanks you for using this plugin. Please see http://www.foxy-shop.com/ for 
+installation instructions and lots of helpful advice on how to get
+the most out of FoxyShop.
+
+**************************************************************************/
+
+//Setup Plugin Variables
+define('FOXYSHOP_VERSION', "2.8");
+define('FOXYSHOP_DIR',WP_PLUGIN_URL."/foxyshop");
+define('FOXYSHOP_PATH', dirname(__FILE__));
+$foxyshop_document_root = $_SERVER['DOCUMENT_ROOT'];
+if ($foxyshop_document_root == "" || $foxyshop_document_root == "/") $foxyshop_document_root = str_replace("/wp-content/plugins/foxyshop","",FOXYSHOP_PATH);
+if (!defined('FOXYSHOP_DOCUMENT_ROOT')) define('FOXYSHOP_DOCUMENT_ROOT', $foxyshop_document_root);
+if (!defined('FOXYSHOP_PRODUCTS_SLUG')) define('FOXYSHOP_PRODUCTS_SLUG','products');
+if (!defined('FOXYSHOP_PRODUCT_CATEGORY_SLUG')) define('FOXYSHOP_PRODUCT_CATEGORY_SLUG','product-cat');
+if (!defined('FOXYSHOP_JQUERY_VERSION')) define('FOXYSHOP_JQUERY_VERSION','1.6.2');
+$foxyshop_settings = unserialize(get_option("foxyshop_settings"));
+if (!is_array($foxyshop_settings)) $foxyshop_settings = array("domain" => "", "sort_key" => "", "enable_sso" => "", "generate_feed" => "", "manage_inventory_levels" => "", "enable_subscriptions" => "");
+if (!array_key_exists('foxyshop_version',$foxyshop_settings)) $foxyshop_settings['foxyshop_version'] = '';
+$foxyshop_category_sort = unserialize(get_option('foxyshop_category_sort'));
+
+//Checks for Old Plugin Version and Perform Upgrade
+if ($foxyshop_settings['foxyshop_version'] != FOXYSHOP_VERSION) add_action('admin_init', 'foxyshop_activation');
+
+//Sets the Locale for Currency Internationalization
+setlocale(LC_MONETARY, (array_key_exists('locale_code',$foxyshop_settings) ? $foxyshop_settings['locale_code'] : get_locale()));
+$foxyshop_localsettings = localeconv();
+if ($foxyshop_localsettings['int_curr_symbol'] == "") setlocale(LC_MONETARY, 'en_US');
+
+//Flushes Rewrite Rules if Structure Has Changed
+add_action('init', 'foxyshop_check_rewrite_rules', 99);
+
+//Widgets and Shortcodes support
+include('widgetcode.php');
+include('shortcodesettings.php');
+
+//Put FoxyCart includes and jQuery on public pages
+if (!is_admin()) {
+	if ($foxyshop_settings['use_jquery']) add_action('init', 'foxyshop_insert_jquery');
+	add_action('wp_head', 'foxyshop_insert_foxycart_files');
+}
+
+//Put FoxyShop Styles on front pages and load in the admin styles as well
+if (is_admin()) {
+	wp_enqueue_style('foxyshop_admin_css', FOXYSHOP_DIR . '/css/foxyshop-admin.css');
+} else {
+	wp_enqueue_style('foxyshop_css', FOXYSHOP_DIR . '/css/foxyshop.css');
+	if ($foxyshop_settings['ga']) add_action('wp_footer', 'foxyshop_insert_google_analytics', 100);
+}
+
+//Custom Post Type and Taxonomy
+include_once('customposttype.php');
+
+//Custom Product Sorting
+include_once('customsorting.php');
+
+//Custom Category Sorting
+include_once('categorysorting.php');
+
+//FoxyCart API Feeds
+if ($foxyshop_settings['domain']) {
+
+	//Orders
+	include_once('orders.php');
+
+	//Customers
+	include_once('customers.php');
+
+	//Subscriptions
+	if ($foxyshop_settings['enable_subscriptions']) {
+		include_once('subscriptions.php');
+	}
+}
+
+//Inventory Management
+if ($foxyshop_settings['manage_inventory_levels']) {
+	include_once('inventory.php');
+}
+
+//Generate Product Feed
+if ($foxyshop_settings['generate_feed']) {
+	include_once('productfeed.php');
+}
+
+//Settings Page
+include_once('settings.php');
+
+//Single Sign On
+if ($foxyshop_settings['enable_sso']) {
+	include_once('sso.php');
+}
+
+//Display Settings Link on Plugin Screen
+add_filter('plugin_action_links', 'foxyshop_plugin_action_links', 10, 2);
+
+//Admin Functions
+include('adminfunctions.php');
+include('adminajax.php');
+
+//Frontend Helper Functions
+include('helperfunctions.php');
+
+//Template Redirect (files are in /themefiles/)
+include('templateredirect.php');
+
+
+//PLUGIN EXTENSION
+//Put your extended functionality in wp-content/foxyshop-custom-functions.php
+//and it will be executed here on plugin load and protected from upgrade overwriting.
+//NOTE, this is not currently supported for security reasons. Input on this potential
+//feature would be great.
+//if (file_exists(WP_CONTENT_DIR.'/foxyshop-custom-functions.php')) include(WP_CONTENT_DIR.'/foxyshop-custom-functions.php');
+
+//Plugin Activation Functions
+register_activation_hook(__FILE__, 'foxyshop_activation');
+register_deactivation_hook( __FILE__, 'foxyshop_deactivation' );
+?>
---
+++ themefiles/foxyshop-single-product.php
@@ -0,0 +1,95 @@
+<?php get_header(); ?>
+
+<?php
+//Hide page content if plugin is disabled
+if (function_exists('foxyshop_insert_foxycart_files')) {
+?>
+<div id="foxyshop_container">
+<?php
+foxyshop_include('header');
+while (have_posts()) : the_post();
+	
+	//PrettyPhoto Includes (can be removed if you want to use a different javascript slideshow plugin)
+	echo '<script type="text/javascript" src="' . FOXYSHOP_DIR . '/js/prettyphoto/jquery.prettyPhoto.js"></script>'."\n";
+	echo '<link rel="stylesheet" href="' . FOXYSHOP_DIR . '/js/prettyphoto/prettyPhoto.css" type="text/css" media="screen" />'."\n";
+	?><script type="text/javascript">jQuery(document).ready(function($){$("a[rel^='foxyshop_gallery']").prettyPhoto({theme: 'light_square', overlay_gallery: false, social_tools: ''});});</script><?php
+
+	//Initialize Product
+	global $product;
+	$product = foxyshop_setup_product();
+	
+	//This is for testing to see what is included in the $product array
+	//print_r($product);
+	
+	//Initialize Form
+	foxyshop_start_form();
+	
+	//Write Breadcrumbs
+	foxyshop_breadcrumbs(" &raquo; ", "&laquo; Back to Products");
+	
+
+	//Show the Main Image and Slideshow if Available
+	$mediumSRC = foxyshop_get_main_image("medium");
+	$mediumSRCtitle = foxyshop_get_main_image("title");
+	$largeSRC = foxyshop_get_main_image("large");
+	$imagecount = count($product['images']);
+	if ($mediumSRC) {
+		echo '<div class="foxyshop_product_image">';
+		if ($mediumSRC != $largeSRC || $imagecount > 1) echo '<a href="' . $largeSRC . '" rel="foxyshop_gallery' . ($imagecount > 1 ? '[fs_gall]' : '') . '"  title="">';
+		echo '<img src="' . $mediumSRC . '" id="foxyshop_main_product_image" alt="' . htmlspecialchars($mediumSRCtitle) . '" title="" />';
+		if ($mediumSRC != $largeSRC || $imagecount > 1) echo '</a>';
+		foxyshop_image_slideshow("thumbnail", false, "Click Below For More Images:");
+		echo "</div>\n";
+	}
+				
+	
+	//Main Product Information Area
+	echo '<div class="foxyshop_product_info">';
+	edit_post_link('<img src="' . FOXYSHOP_DIR . '/images/editicon.png" alt="Edit Product" width="16" height="16" />','<span class="foxyshop_edit_product">','</span>');
+	echo '<h2>' . apply_filters('the_title', $product['name']) . '</h2>';
+	
+	//Show a sale tag if the product is on sale
+	//if (foxyshop_is_on_sale()) echo '<p>SALE!</p>';
+
+	//Product Is New Tag (number of days since added)
+	//if (foxyshop_is_product_new(14)) echo '<p>NEW!</p>';
+	
+	//Main Product Description
+	echo $product['description'];
+
+
+	//Show Variations (showQuantity: 0 = Do Not Show Qty, 1 = Show Before Variations, 2 = Show Below Variations)
+	foxyshop_product_variations(2);
+	
+	//(style) clear floats before the submit button
+	echo '<div class="clr"></div>';
+
+	//Check Inventory Levels and Display Status (last variable allows ordering of out of stock items)
+	foxyshop_inventory_management("There are only %c item%s left in stock.", "Item is not in stock.", false);
+	
+	//Add To Cart Button
+	echo '<button type="submit" name="x:productsubmit" id="productsubmit" class="foxyshop_button">Add To Cart</button>';
+	
+	//Shows the Price (includes sale price if applicable)
+	echo '<div id="foxyshop_main_price">';
+	foxyshop_price();
+	echo '</div>';
+
+	//Shows any related products
+	foxyshop_related_products("Related Products");
+
+
+	//Ends the form
+	echo '</div>';
+	echo '</form>';
+
+
+endwhile;
+?>
+	<div class="clr"></div>
+	<?php foxyshop_include('footer'); ?>
+</div>
+<?php } ?>
+
+<?php
+get_footer(); ?>
